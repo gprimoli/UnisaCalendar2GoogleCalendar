ce76e04e563a7b994523393f04be138b89cdc4ff
super99iper <super99iper@gmail.com>
2020-02-29 16:31:45 +0100

    Delete semestreTerminatoException.java

--- semestreTerminatoException.java
+++
@@ -1,9 +0,0 @@
-public class semestreTerminatoException extends Exception{
-    public semestreTerminatoException(String message) {
-        super(message);
-    }
-
-    public semestreTerminatoException(int semestre) {
-        super("Hai terminato il " + semestre + "°");
-    }
-}
